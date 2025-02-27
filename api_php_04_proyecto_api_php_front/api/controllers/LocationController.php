8632c938a7b936c51f6dda3ed8ae5ee33023a1cf
SenaProfeAlbeiro <earamos42@misena.edu.co>
2025-02-27 06:56:40 -0500

    proyecto_api_php_front

--- api_php_04_proyecto_api_php_front/api/controllers/LocationController.php
+++ api_php_04_proyecto_api_php_front/api/controllers/LocationController.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../models/Location.php';
+require_once 'models/Location.php';
 
 class LocationController {
     private $location;
--- api_php_04_proyecto_api_php_front/api/routes.php
+++ api_php_04_proyecto_api_php_front/api/routes.php
@@ -1,7 +1,7 @@
 <?php
-require_once '../core/Router.php';
 require_once 'controllers/LocationController.php';
-require_once 'config/conexion.php';
+require_once '../core/Router.php';
+require_once 'config/conexion.php'; // Incluir el archivo conexion.php
 
 $conn = conexion(); // Obtener la conexión a la base de datos
 //Verificamos que la conexion se realizo correctamente.
@@ -17,4 +17,5 @@ if($conn){
 
     $router->route();
 }
+
 ?>
