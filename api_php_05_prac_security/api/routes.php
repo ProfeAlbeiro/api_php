df66f3d06c2f00aa4416d44384fea7af64862b0f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-02-28 17:58:03 -0500

    api_php_proyecto03

---
+++ api_php_05_prac_security/api/routes.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'controllers/LocationController.php';
+require_once '../core/Router.php';
+require_once 'config/DataBase.php'; // Incluir el archivo conexion.php
+
+$conn = DataBase::conexion(); // Obtener la conexión a la base de datos
+//Verificamos que la conexion se realizo correctamente.
+if($conn){
+
+    $router = new Router();
+
+    $router->addRoute('GET', '/locations', 'LocationController', 'listarLocations', $conn);
+    $router->addRoute('GET', '/locations/{id}', 'LocationController', 'obtenerLocation', $conn);
+    $router->addRoute('POST', '/locations', 'LocationController', 'crearLocation', $conn);
+    $router->addRoute('PUT', '/locations/{id}', 'LocationController', 'actualizarLocation', $conn);
+    $router->addRoute('DELETE', '/locations/{id}', 'LocationController', 'eliminarLocation', $conn);
+
+    $router->route();
+}
+
+?>
