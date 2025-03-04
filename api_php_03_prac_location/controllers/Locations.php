b8e77911b60756c64a36a79f05e92d5b99991084
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-04 16:34:02 -0500

    api_php_proyecto03

--- api_php_03_prac_location/controllers/Locations.php
+++ api_php_03_prac_location/controllers/Locations.php
@@ -1,4 +1,7 @@
 <?php
+    header("Access-Control-Allow-Origin: http://localhost:4200");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header('Content-Type: application/json');
 
     require_once "../config/DbConn.php";
